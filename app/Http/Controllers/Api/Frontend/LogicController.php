f21180236e247561685865d8152c5bb5f1fb9198
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-01-26 18:17:15 +0600

    u

--- app/Http/Controllers/Api/Frontend/LogicController.php
+++ app/Http/Controllers/Api/Frontend/LogicController.php
@@ -16,13 +16,13 @@ class LogicController extends Controller
             return Helper::jsonResponse(false, 'Logic not found', 404);
         }
         $data = [
-            'multi_trip_standard' => $logic->multi_trip_standard,
-            'multi_trip_extended' => $logic->multi_trip_extended,
-            'cancellation_coverage_standard' => $logic->cancellation_coverage_standard,
-            'cancellation_coverage_extended' => $logic->cancellation_coverage_extended,
-            'winter_sports' => $logic->winter_sports,
-            'adventure_sports_multi' => $logic->adventure_sports_multi,
-            'adventure_sports_single' => $logic->adventure_sports_single,
+            'multi_trip_standard' => ($logic->multi_trip_standard / 100) + 1,
+            'multi_trip_extended' => ($logic->multi_trip_extended / 100) + 1,
+            'cancellation_coverage_standard' => ($logic->cancellation_coverage_standard / 100) + 1 ,
+            'cancellation_coverage_extended' => ($logic->cancellation_coverage_extended / 100) + 1,
+            'winter_sports' => ($logic->winter_sports + 100) / 100,
+            'adventure_sports_multi' => ($logic->adventure_sports_multi + 100) / 100,
+            'adventure_sports_single' => ($logic->adventure_sports_single + 100) / 100,
             'charge' => $logic->charge
         ];
         return Helper::jsonResponse(true, 'Logic', 200, $data);
