40802f5429fa6bb7aeaf539de2d239b6d4ca179c
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-01-26 18:13:24 +0600

    u

--- app/Http/Controllers/Api/Frontend/LogicController.php
+++ app/Http/Controllers/Api/Frontend/LogicController.php
@@ -15,6 +15,16 @@ class LogicController extends Controller
         if (!$logic) {
             return Helper::jsonResponse(false, 'Logic not found', 404);
         }
-        return Helper::jsonResponse(true, 'Logic', 200, $logic);
+        $data = [
+            'multi_trip_standard' => $logic->multi_trip_standard,
+            'multi_trip_extended' => $logic->multi_trip_extended,
+            'cancellation_coverage_standard' => $logic->cancellation_coverage_standard,
+            'cancellation_coverage_extended' => $logic->cancellation_coverage_extended,
+            'winter_sports' => $logic->winter_sports,
+            'adventure_sports_multi' => $logic->adventure_sports_multi,
+            'adventure_sports_single' => $logic->adventure_sports_single,
+            'charge' => $logic->charge
+        ];
+        return Helper::jsonResponse(true, 'Logic', 200, $data);
     }
 }
