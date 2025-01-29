7cd4a3e1c708c3fcf19cbd4f03992fb1bc005b51
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-01-29 17:32:01 +0600

    u

--- app/Http/Controllers/Api/Frontend/PriceController.php
+++ app/Http/Controllers/Api/Frontend/PriceController.php
@@ -25,21 +25,30 @@ class PriceController extends Controller
         }
 
         $priceQuery = Pricing::where('is_annual', $request->is_annual)->where('destination', $request->destination);
+       
+        $maxDuration = $request->max_duration;
 
-        if ($request->max_duration > 30) {
-            return Helper::jsonResponse(false, 'Price not found', 404);
-        } elseif ($request->max_duration > 1 && $request->max_duration <= 7) { //1-7 = 7
-            $priceQuery->where('max_duration', 7);
-        } elseif ($request->max_duration > 8 && $request->max_duration <= 10) { //8-10 = 10
-            $priceQuery->where('max_duration', 10);
-        } elseif ($request->max_duration >= 11 && $request->max_duration <= 18) { //11-18 = 18
-            $priceQuery->where('max_duration', 18);
-        } elseif ($request->max_duration >= 19 && $request->max_duration <= 24) { //19-24 = 24
-            $priceQuery->where('max_duration', 24);
-        } elseif ($request->max_duration >= 25 && $request->max_duration <= 30) { //25-30 = 30
-            $priceQuery->where('max_duration', 30);
-        } else {
-            $priceQuery->where('max_duration', 0);
+        switch (true) {
+            case $maxDuration == 0 || $maxDuration == "0" :
+                $priceQuery->where('max_duration', 0)->orWhere('max_duration', '0');
+                break;
+            case $maxDuration >= 1 && $maxDuration <= 7:
+                $priceQuery->where('max_duration', 7);
+                break;
+            case $maxDuration > 8 && $maxDuration <= 10:
+                $priceQuery->where('max_duration', 10);
+                break;
+            case $maxDuration >= 11 && $maxDuration <= 18:
+                $priceQuery->where('max_duration', 18);
+                break;
+            case $maxDuration >= 19 && $maxDuration <= 24:
+                $priceQuery->where('max_duration', 24);
+                break;
+            case $maxDuration >= 25 && $maxDuration <= 30:
+                $priceQuery->where('max_duration', 30);
+                break;
+            default:
+                return Helper::jsonResponse(false, 'Price not found', 404);
         }
 
         $price = $priceQuery->where('age_group', $request->age_group)->where('party_type', $request->party_type)->first();
