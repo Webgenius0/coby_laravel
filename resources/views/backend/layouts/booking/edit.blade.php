acb67ac9c08a8a7f54e53bf338d631c75775df68
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-01-29 15:14:17 +0600

    u

--- resources/views/backend/layouts/booking/edit.blade.php
+++ resources/views/backend/layouts/booking/edit.blade.php
@@ -132,6 +132,25 @@
                                             </div>
 
                                             <div class="form-group">
+                                                <label for="travel_type" class="form-label">Travel Type:</label>
+                                                <div>
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input @error('travel_type') is-invalid @enderror" name="travel_type[]" value="adventure" {{ is_array(old('travel_type', $booking->travel_type)) && in_array('adventure', old('travel_type', $booking->travel_type)) ? 'checked' : '' }}>
+                                                        Winter Sports?
+                                                    </label>
+                                                </div>
+                                                <div>
+                                                    <label class="form-check-label">
+                                                        <input type="checkbox" class="form-check-input @error('travel_type') is-invalid @enderror" name="travel_type[]" value="leisure" {{ is_array(old('travel_type', $booking->travel_type)) && in_array('leisure', old('travel_type', $booking->travel_type)) ? 'checked' : '' }}>
+                                                        Adventure Sports?
+                                                    </label>
+                                                </div>
+                                                @error('travel_type')
+                                                <span class="text-danger">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="form-group">
                                                 <button class="btn btn-primary" type="submit">Submit</button>
                                             </div>
 
