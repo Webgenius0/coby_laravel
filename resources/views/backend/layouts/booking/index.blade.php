12bec3de68400cabfd19f3f02bcd2c0dfc4ba25a
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-01-29 11:44:42 +0600

    u

--- resources/views/backend/layouts/booking/index.blade.php
+++ resources/views/backend/layouts/booking/index.blade.php
@@ -34,8 +34,8 @@
                     <div class="card product-sales-main">
                         <div class="card-header border-bottom">
                             <div class="btn-group" role="group" aria-label="Basic example">
-                                <button onclick="window.location.reload({{ route('booking.index', 'paid') }})" type="button" class="btn btn-primary">Paid</button>
-                                <button onclick="window.location.reload({{ route('booking.index', 'unpaid') }})" type="button" class="btn btn-primary">Unpaid</button>
+                                <button onclick="window.location.reload({{ route('insurance.booking.index', 'paid') }})" type="button" class="btn btn-primary">Paid</button>
+                                <button onclick="window.location.reload({{ route('insurance.booking.index', 'unpaid') }})" type="button" class="btn btn-primary">Unpaid</button>
                             </div>
                         </div>
                         <div class="card-body">
