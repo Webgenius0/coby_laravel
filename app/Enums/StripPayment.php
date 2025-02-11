e1b835618592eecd548be883d2914a8ecc7c4ebf
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-02-11 18:11:34 +0600

    u

--- app/Enums/StripPayment.php
+++ app/Enums/StripPayment.php
@@ -4,6 +4,6 @@ namespace App\Enums;
 
 enum StripPayment: string
 {
-    case SUCCESS   = 'https://journeyman-services.netlify.app/success';
-    case FAIL      = 'https://journeyman-services.netlify.app/error';
+    case SUCCESS   = 'https://journeyman-services-web.netlify.app/success';
+    case FAIL      = 'https://journeyman-services-web.netlify.app/error';
 }
