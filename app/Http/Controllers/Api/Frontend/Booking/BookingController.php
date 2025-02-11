9ea0f78bf4bb9bb134fec43ab1f95067d345d777
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-02-11 18:08:44 +0600

    u

--- app/Http/Controllers/Api/Frontend/Booking/BookingController.php
+++ app/Http/Controllers/Api/Frontend/Booking/BookingController.php
@@ -81,8 +81,8 @@ class BookingController extends Controller
 
         $data = Booking::create($validatedData);
 
-        $this->BookingServices->jsonFileSave($data);
-        $this->BookingServices->PdfFileSave($data);
+        /* $this->BookingServices->jsonFileSave($data);
+        $this->BookingServices->PdfFileSave($data); */
 
         return redirect()->route('payment.stripe.checkout', $data->id);
     }
